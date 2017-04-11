e7c49a2fa1c6534bccc385728f8826be98aed9a3
Jeronimo Mora <jeronimomora@berkeley.edu>
2017-04-11 11:36:04 -0700

    Plane thing closes #1

--- src/index.js
+++ src/index.js
@@ -16,13 +16,18 @@ camera.position.set(-70,40,30);
 camera.up = new THREE.Vector3(0,1,0);
 camera.lookAt(new THREE.Vector3(0,0,20));
 
-var geometry = new THREE.PlaneGeometry(50,50);
+var planeW = 50;
+var planeH = 50;
+
+var geometry = new THREE.PlaneGeometry(planeW,planeH);
 var pmaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, side: THREE.DoubleSide});
 var plane = new THREE.Mesh( geometry, pmaterial );
 plane.lookAt(new THREE.Vector3(0, 1, 0));
-plane.position.set(0,0,26);
-scene.add( plane );
 
+var planeZ = (planeH)/2 + 1;
+plane.position.set(0,0,planeZ);
+
+scene.add( plane );
 
 var plane = new THREE.Plane
 var render = function () {
